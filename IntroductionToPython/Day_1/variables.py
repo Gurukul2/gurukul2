bf1c66d89df524401ecb285e40546d379dca7f1b
Shrivastava Kalpit <kalpit.shrivastava@nokia.com>
2024-09-16 11:09:52 +0530

    FlowControl and Operators

--- IntroductionToPython/Day_1/variables.py
+++ IntroductionToPython/Day_1/variables.py
@@ -1,14 +1,32 @@
-123total= "nokia"
-total123="nokia"
-java2share="nokia"
-ca$h="nokia"
-_abc_abc_="nokia"
-Def="nokia"
-if="nokia"
 
+# # Global variable
+# x = 10
+# def my_function():
+#     # Local variable
+#     y = 5
+#     print("Local variable y:", y)
+#
+#     # Accessing global variable
+#     print("Global variable x inside function:", x)
+#
+#
+# my_function()
+#
+# # Accessing global variable outside the function
+# print("Global variable x outside function:", x)
 
-A = 10
-print(A)
+# Trying to access local variable outside the function will raise an error
+# print(y)  # This will cause an error: NameError: name 'y' is not defined
 
-import keyword
-print(keyword.kwlist)
+# 123total= "nokia"
+# total123="nokia"
+# java2share="nokia"
+# ca$h="nokia"
+# _abc_abc_="nokia"
+# Def="nokia"
+# if="nokia"
+#
+#
+#
+# import keyword
+# print(keyword.kwlist)
